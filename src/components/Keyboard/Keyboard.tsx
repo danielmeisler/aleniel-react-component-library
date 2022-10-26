dcba5e71c5f13eec5c1ba3b8a8bf8d2dc5128934
Daniel <daniel_meisler@web.de>
2022-10-26 15:43:18 +0200

    feat: added keyboard

---
+++ src/components/Keyboard/Keyboard.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./Keyboard.css";
+
+
+const Keyboard = () => {
+  return <div className='Keyboard'></div>;
+};
+
+
+export default Keyboard;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,2 @@
-export { default as Akkordeon } from "./Akkordeon";
 export { default as Button } from "./Button";
+export { default as Keyboard } from "./Keyboard";

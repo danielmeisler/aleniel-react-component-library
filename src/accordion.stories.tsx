1d2f702b6ddf854a4061aff047c10b2c1ec641a8
Alexander <wakup.alexander@gmx.de>
2022-10-31 11:50:21 +0100

    added controls

--- src/accordion.stories.tsx
+++ src/accordion.stories.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
+import type { Story } from "@ladle/react";
 import { Accordion as Acc }from './../lib/src/components/';
 
 
-export const Accordion = () => <Acc title="Titel" content='Test'></Acc>
-export const AccordionOpen = () => <Acc title="Titel" content='Test'></Acc>
+
+export const Accordion = () => <Acc isOpen={false} title="Titel" content='Test'></Acc>
+export const AccordionOpen = () => <Acc isOpen={true} title="Titel" content='Test'></Acc>
+
+
+export const AccordionWithControls: Story<{
+  isOpen: boolean;
+  title: string;
+  content: string;
+}> = ({isOpen, title, content}) => <Acc isOpen={isOpen} title={title} content={content}></Acc>
+
+
+AccordionWithControls.args = {
+  isOpen: false,
+  title: "Titel",
+  content: "Inhalt"
+};

1cf81e5c5488221b80e78b35a45ccebecdfb0b7c
Alexander <wakup.alexander@gmx.de>
2022-10-31 11:24:42 +0100

    merged ladle, added isOpen to acc

--- lib/src/components/Accordion/Accordion.tsx
+++ lib/src/components/Accordion/Accordion.tsx
@@ -4,10 +4,11 @@ import "./Accordion.css";
 export interface AccordionProps {
   title: string;
   content: string;
+  isOpen: boolean;
 }
 
 const Accordion = (props: AccordionProps) => {
-  const [isActive, setIsActive] = useState(false);
+  const [isActive, setIsActive] = useState(props.isOpen);
   return (
     <div className='accordianContainer'>
       <div className='accordionHeader' onClick={() => { setIsActive(!isActive) }} > {props.title}

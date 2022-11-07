45e512a663467461f5c340ec41df9e0001f2e968
Alex <alexander.reiprich@hs-furtwangen.de>
2022-11-07 15:36:50 +0100

    fix: vitest errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
-    include: ['./components/**/*.test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    include: ['./src/components/**/*.test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     globals: true,
     environment: 'jsdom',
   }

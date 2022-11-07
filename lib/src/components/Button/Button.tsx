c106322a11191a06fd41ae936d49b9471992f0b9
Alex <alexander.reiprich@hs-furtwangen.de>
2022-11-07 09:35:12 +0100

    feat: theming for button

--- lib/src/components/Button/Button.tsx
+++ lib/src/components/Button/Button.tsx
@@ -1,12 +1,24 @@
+import Cookies from 'js-cookie';
 import styles from './Button.module.css';
 import React, { FunctionComponent } from 'react';
 
-const Button: FunctionComponent<ButtonProps> = ({ label }) => (
-  <button className={ `${styles.AlenielButton}` }>{ label }</button>
-);
+const Button: FunctionComponent<ButtonProps> = ({ label, theme }) => {
+  let curTheme = '';
+
+  if (theme === undefined) {
+    curTheme = Cookies.get('theme')!;
+  }
+
+  if (theme === 'dark' || curTheme === 'dark') {
+    return (<button className={ `${styles.AlenielButtonDark}` }>{ label }</button>);
+  }
+
+  return (<button className={ `${styles.AlenielButtonLight}` }>{ label }</button>);
+};
 
 export interface ButtonProps {
   label: string;
+  theme?: string;
 }
 
 export { Button };

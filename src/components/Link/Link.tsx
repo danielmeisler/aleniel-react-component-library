1e3ac7e7c6041e931f8fee48cab502cc8d4f1ea1
Daniel <daniel_meisler@web.de>
2022-11-23 11:49:31 +0100

    feat: new version added link component wip

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -7,7 +7,8 @@ type LinkProps = {
   { smoothScrollToId: string } |
   { mailTo: string } |
   { tel: string } |
-  { externalLink: string }
+  { externalLink: string } |
+  { internalLink: string }
 );
 
 /**
@@ -21,15 +22,18 @@ type LinkProps = {
  * @param externalLink
  * Opens a new tab with the rel='noopener noreferrer' attribute.
  * Use an available link to a website. Example: "https://google.com"
+ * @param internalLink
+ * Switches between pages inside an application without reloading.
+ * Use an existent path of one of your pages. Uses the a-tag href prefix /.
  * @param smoothScrollToId
  * Anchor Tag which scrools smoothly to the same ID.
- * Use an existent id from your components.
+ * Use an existent id from your components. Uses the a-tag href prefix #.
  * @param mailTo
  * One click to write an e-mail.
- * Put the recipient email address in.
+ * Put the recipient email address in. Uses the a-tag href prefix mailto.
  * @param tel
  * One click to call someone.
- * Put the recipient phone number in.
+ * Put the recipient phone number in. Uses the a-tag href prefix tel.
  */
 
 const Link: FunctionComponent<LinkProps> = ({ label, ...props }) => {
@@ -49,6 +53,11 @@ const Link: FunctionComponent<LinkProps> = ({ label, ...props }) => {
   if ('tel' in props && !(props.tel === '')) {
     href = `tel:${props.tel}`;
     LinkComponent = <a className={ `${styles.Link}` } href={ href }> &#9743; {label ?? props.tel} </a>;
+  }
+
+  if ('internalLink' in props && !(props.internalLink === '')) {
+    href = `/${props.internalLink}`;
+    LinkComponent = <a className={ `${styles.Link}` } href={ href }> &#10151; { label ?? props.internalLink } </a>;
   }
 
   if ('externalLink' in props && !(props.externalLink === '')) {

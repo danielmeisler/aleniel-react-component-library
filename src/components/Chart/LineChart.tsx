a4f6ef2040d0bf2f1a1ee8edd5cc84d3419ce04e
Alexander <wakup.alexander@gmx.de>
2022-12-01 12:19:29 +0100

    refactor, added x axis labels

--- src/components/Chart/LineChart.tsx
+++ src/components/Chart/LineChart.tsx
@@ -7,23 +7,68 @@ interface LineChartProps {
   spacing?: number;
 }
 
-export const LineChart: FunctionComponent<LineChartProps> = ({ width = 500, height = 100, spacing = 10 }) => {
-  const datapoints: number[] = [ 50, 4, 100, 20, 25, 4, 20, 80, 40, 100, 20, 5, 15, 80, 20, 4, 10, 50, 20, 3, 25 ];
+export const LineChart: FunctionComponent<LineChartProps> = ({ width = 500, height = 100, spacing = 20 }) => {
+  const dataset = [
+    {
+      data: 50,
+      label: 'now'
+    },
+    {
+      data: 4,
+      label: '12:00'
+    },
+    {
+      data: 100,
+      label: '13:00'
+    },
+    {
+      data: 20,
+      label: '14:00'
+    },
+    {
+      data: 25,
+      label: '15:00'
+    },
+    {
+      data: 5,
+      label: '16:00'
+    },
+    {
+      data: 90,
+      label: '17:00'
+    },
+    {
+      data: 20,
+      label: '18:00'
+    },
+    {
+      data: 60,
+      label: '19:00'
+    },
+    {
+      data: 24,
+      label: '20:00'
+    },
+    {
+      data: 80,
+      label: '21:00'
+    }
+  ];
 
-  const newDatapoints: number[] = [];
+  const datapoints: number[] = [];
   const yOffset = 40;
   const xOffset = 60;
   let path = `M${500 - (xOffset / 2)} ${100 + (yOffset / 2)} `;
 
   // eslint-disable-next-line unicorn/no-for-loop
-  for (let i = 0; i < datapoints.length; i++) {
-    newDatapoints.push(width - (i * spacing) - (xOffset / 2));
-    newDatapoints.push(height - datapoints[i] + (yOffset / 2));
+  for (let i = 0; i < dataset.length; i++) {
+    datapoints.push(width - (i * spacing) - (xOffset / 2));
+    datapoints.push(height - dataset[i].data + (yOffset / 2));
     path += `L${width - (i * spacing) - (xOffset / 2)} `;
-    path += `${height - datapoints[i] + (yOffset / 2)} `;
+    path += `${height - dataset[i].data + (yOffset / 2)} `;
   }
 
-  path += `${width - (xOffset / 2) - ((datapoints.length - 1) * spacing)} ${height + (yOffset / 2)} Z`;
+  path += `${width - (xOffset / 2) - ((dataset.length - 1) * spacing)} ${height + (yOffset / 2)} Z`;
 
   return (
     <svg viewBox={ `0 0 ${width} ${height + yOffset}` } className={ styles.linechart }>
@@ -38,7 +83,7 @@ export const LineChart: FunctionComponent<LineChartProps> = ({ width = 500, heig
         fill='none'
         stroke='#35D5E4'
         strokeWidth='3'
-        points={ newDatapoints.toString() }
+        points={ datapoints.toString() }
       />
 
       <line x1={ xOffset / 2 } y1={ height + (yOffset / 2) } x2={ width - (xOffset / 2) } y2={ height + (yOffset / 2) } stroke='#232527' />
@@ -49,9 +94,13 @@ export const LineChart: FunctionComponent<LineChartProps> = ({ width = 500, heig
         <tspan x='25' y={ 3 + (height / 2) + (yOffset / 2) }>50%</tspan>
         <tspan x='25' y={ 3 + height + (yOffset / 2) }>0%</tspan>
       </text>
-      <g className={ styles.label }>
-        <text x={ width - (xOffset / 2) } y={ height + (yOffset / 2) + 14 }>Now</text>
-      </g>
+      <text className={ styles.label } textAnchor='middle'>
+        {
+          dataset.map((datapoint, index) =>
+            index % 2 === 0 &&
+            <tspan key={ datapoint.label.toString() } x={ (width - (yOffset / 2) - (spacing / 2)) - (index * spacing) } y={ height + (yOffset / 2) + 14 }>{ datapoint.label }</tspan>)
+        }
+      </text>
     </svg>
   );
 };
